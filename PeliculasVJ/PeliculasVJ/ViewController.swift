d14741f2fabd567a34c0708830434ce3504ffdd2
B303-19 <administrador@b303-19.local>
2016-10-20 12:18:38 -0500

    Inicio del proyecto que debe hacer hace 3 semanas!

---
+++ PeliculasVJ/PeliculasVJ/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PeliculasVJ
+//
+//  Created by B303-19 on 20/10/16.
+//  Copyright © 2016 B303-19. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
